9faf70d7dea50c77e46926beaafad4da9e55bb95
Rahul Sahu <rahulselectandswitch@gmail.com>
2025-08-20 15:08:44 +0000

    keep home page as orginal one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { Header } from '@/components/layout/header';
 import { Hero } from '@/components/sections/hero';
+import { Highlights } from '@/components/sections/highlights';
+import { MusicalStyle } from '@/components/sections/musical-style';
+import { Albums } from '@/components/sections/albums';
 import { Footer } from '@/components/layout/footer';
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
       <Header />
       <main className="flex-grow">
         <Hero />
+        <Highlights />
+        <MusicalStyle />
+        <Albums />
       </main>
       <Footer />
     </div>
